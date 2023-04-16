784d09225436e5da254703a1fc96a48c9d4b911a
JOZSEF-KISS <vona.2015@gmail.com>
2023-04-16 14:04:50 +0200

    main.js 2023.04.16.

--- Assets/main.js
+++ Assets/main.js
@@ -13,3 +13,25 @@ close.addEventListener("click", closePopup);
 function closePopup() {
   document.getElementById("popup-js").classList.toggle("popup-container");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Az összes menüpontra kattintáskor elrejtjük a menüt és a hivatkozáshoz ugrunk
+  var menuItems = document.querySelectorAll(".menu__item");
+  menuItems.forEach(function (item) {
+    item.addEventListener("click", function (event) {
+      var menuToggle = document.getElementById("menu-toggle");
+      menuToggle.checked = false; // Menü elrejtése
+
+      // Hivatkozásra ugrás, csak akkor, ha a kattintás egy hivatkozáson belül történt
+      if (event.target.tagName === "A") {
+        var href = event.target.getAttribute("href");
+        if (href.startsWith("#")) {
+          // Ellenőrizzük, hogy az ugrás egy belső horgonyra történik-e
+          event.preventDefault(); // Megakadályozzuk az alapértelmezett viselkedést (az ugrást)
+          var targetElement = document.querySelector(href);
+          targetElement.scrollIntoView({ behavior: "smooth" }); // Sima görgetés a horgonyhoz
+        }
+      }
+    });
+  });
+});
